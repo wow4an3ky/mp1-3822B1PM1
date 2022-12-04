2e9b10c4adce878f1eaba57eaf3804067fb4b0cb
wow4an3ky <vovankub13@mail.ru>
2022-12-04 18:24:50 +0300

    Initial structure

---
+++ file/lab/lab.c
@@ -0,0 +1,38 @@
+﻿#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+void bubble(float* arr, int N)
+{
+	float tmp;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N-i-1 ; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
+int main()
+{
+	float a[10];
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		a[i] = rand();
+		printf("%d - %f\n", i, a[i]);
+	}
+	bubble(a, 10);
+	printf("sortirovka:\n");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%f\n",a[i]);
+	}
+	return 0;
+}
